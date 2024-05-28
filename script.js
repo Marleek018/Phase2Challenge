cbab774e3f48bcc2a1e9a5df6e2a391f88082f8d
Marleek018 <Abdulmarleek018@yahoo.com>
2024-05-28 07:55:30 -0700

    hamburger function

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger')
+const mobilenav =document.querySelector('.mobilenav')
+const hambOpen =document.querySelector('.hamburger-open')
+const hambClose =document.querySelector('.close')
+
+function burger() {
+    mobilenav.classList.toggle('mobile-pop');
+    hambOpen.classList.toggle('close-all');
+    hambClose.classList.toggle('close-all');
+    
+}
+hamburger.addEventListener('click', burger)
